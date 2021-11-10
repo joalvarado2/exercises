d689f44ebdd7151e9eed6ee79775710b32a49027
joalvarado2 <jonathanalva92@gmail.com>
2021-11-09 20:57:13 -0500

    agregando una funcionalidad mas en Async

--- async-await.js
+++ async-await.js
@@ -30,37 +30,41 @@ const materias = [
     idMateria: 3,
     materia: "Español",
   },
+  {
+    idMateria: 4,
+    materia: "Sociales",
+  },
 ];
 const cursoNotasPorId = [
-    {
-      nombreCurso: "Matematicas",
-      idEstudiante: 1,
-      nota1: 3,
-      nota2: 4,
-      nota3: 5,
-    },
-    {
-      nombreCurso: "Filosofia",
-      idEstudiante: 2,
-      nota1: 3,
-      nota2: 4,
-      nota3: 5,
-    },
-    {
-      nombreCurso: "Español",
-      idEstudiante: 3,
-      nota1: 3,
-      nota2: 4,
-      nota3: 5,
-    },
-    {
-      nombreCurso: "Sociales",
-      idEstudiante: 4,
-      nota1: 3,
-      nota2: 4,
-      nota3: 5,
-    },
-  ];
+  {
+    nombreCurso: "Matematicas",
+    idEstudiante: 1,
+    nota1: 3,
+    nota2: 4,
+    nota3: 5,
+  },
+  {
+    nombreCurso: "Filosofia",
+    idEstudiante: 2,
+    nota1: 4,
+    nota2: 4,
+    nota3: 4,
+  },
+  {
+    nombreCurso: "Español",
+    idEstudiante: 3,
+    nota1: 2,
+    nota2: 3,
+    nota3: 4,
+  },
+  {
+    nombreCurso: "Sociales",
+    idEstudiante: 4,
+    nota1: 3,
+    nota2: 3,
+    nota3: 5,
+  },
+];
 
 const getEstudiante = (id) => {
   return (promise = new Promise((resolve, reject) => {
@@ -80,11 +84,22 @@ const getMateria = (id) => {
   }));
 };
 
+const getNota = (id) => {
+  return (promise = new Promise((resolve, reject) => {
+    const notas = cursoNotasPorId.find((nota) => nota.idEstudiante == id);
+    notas ? resolve(notas) : reject(`no se encuentran las notas del id ${id}`);
+  }));
+};
+
 const getUsuario = async (id) => {
   try {
     const estudiante = await getEstudiante(id);
     const materia = await getMateria(id);
-    return `el estudiante: ${estudiante} cursa la materia ${materia}`;
+    const notas = await getNota(id);
+    const { nota1, nota2, nota3 } = notas;
+    return `el estudiante: ${estudiante} cursa la materia ${materia} y sus notas fueron: ${
+      (nota1 + nota2 + nota3) / 3
+    }`;
   } catch (error) {
     throw error;
   }
